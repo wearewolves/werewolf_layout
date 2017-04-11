f603348375db25183a92324a9da3521d634e898d
kijuli <rekenber@gmail.com>
2017-04-12 03:41:40 +0900

    관리용 파일들 추가

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-werewolf_layout
+[![Build Status](https://travis-ci.org/wearewolves/werewolf_layout.svg?branch=master)](https://travis-ci.org/wearewolves/werewolf_layout)
+
+1일 마을의 레이아웃 저장소입니다.
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var minimist = require('minimist');
+var args = minimist(process.argv.slice(2));
+gulp.task('deploy', function() {
+  var remotePath = '/www/';
+  var conn = ftp.create({
+    host: 'werewolf5.co.kr',
+    user: args.user,
+    password: args.password,
+    log: gutil.log,
+    maxConnections: 4
+  });
+  gulp.src(['README.md'])
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));
+  gulp.src(['Werewolf/**/*.*'])
+    .pipe(conn.newer(remotePath + 'Werewolf/'))
+    .pipe(conn.dest(remotePath + 'Werewolf/'));
+});
+
